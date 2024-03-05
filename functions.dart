c93a73408eee59e406217de627d07a0e2339b39c
Shea50 <160406484+Shea50@users.noreply.github.com>
2024-03-05 22:43:58 +0300

    Add files via upload

---
+++ functions.dart
@@ -0,0 +1,19 @@
+void main () {
+  int a=101;
+  int b=5;
+
+  int addTwo=a+b;
+  int subtractTwo=a-b;
+  int multiplyTwo=a*b;
+  double divideTwo=a/b;
+  var stringLength = 'Hello';
+  List<int> numbers= [1, 2, 3, 4, 5];
+  int getFirstElement = numbers[0];
+
+  print("the addition is $addTwo");
+  print("the difference is $subtractTwo");
+  print("the multiplication is $multiplyTwo");
+  print("the division is $divideTwo");
+  print(stringLength.length);
+  print(getFirstElement);
+  }
